c2ea802c5d77a897ca5d5cfd6c32f00caa79aae5
w.benarto <w.benarto@gmail.com>
2019-07-03 23:33:21 -0700

    differences

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import { Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron"
 import { Input, TextArea, FormBtn } from "../components/Form";
-// import "./style.css";
+import "./style.css";
 
 class Homepage extends Component {
     render() {
@@ -15,6 +15,10 @@ class Homepage extends Component {
                 <form>
                     <Input name="name" placeholder="Name" />
                     <Input name="gender" placeholder="Gender" />
+                    <Input name="breed" placeholder="Breed" />
+                    <Input name="personality" placeholder="Personality" />
+                    <Input id="desc" name="description" placeholder="Desription" />
+                    
                     {/* <TextArea name="synopsis" placeholder="Synopsis (Optional)" /> */}
                     <FormBtn>Submit</FormBtn>
                 </form>

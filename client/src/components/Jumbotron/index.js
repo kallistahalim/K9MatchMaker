fc1dccc2f2b51473dc9c24422027ce4f2c404373
kallistahalim <kallistahalim@gmail.com>
2019-07-03 22:12:05 -0700

    add model

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./style.css";
+import "../Grid/index"
 
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      style={{ height: 600, clear: "both", paddingTop: 120, textAlign: "center" }}
       className="jumbotron"
     >
       {children}
